f887a98566bba213065a0f2184c0a94e7c0539e2
Clay <clayvanderkolk@gmail.com>
2026-01-06 22:53:56 -1000

    vercel speed checks

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,12 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import AppLayout from "./AppLayout.jsx";
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import "bootstrap/dist/css/bootstrap.min.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <AppLayout />
+      <SpeedInsights />
     </BrowserRouter>
   </StrictMode>
 );
